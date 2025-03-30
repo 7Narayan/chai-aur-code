6f18cc4a4eaddab697ed6b9abb59c16d9ca5d4bb
7Narayan <22eg105e38@anurag.edu.in>
2025-03-30 12:57:51 +0530

    learnt1

--- basics3/arrow3.js
+++ basics3/arrow3.js
@@ -1,23 +1,23 @@
-const user = {
-    username: "hitesh",
-    price: 999,
+// const user = {
+//     username: "hitesh",
+//     price: 999,
 
-    welcomeMessage: function()   // { welcomeMessage: function() { ... } is similar to  const welcomeMessage = function() { ... };
-                                // here "welcomemesssage " is a key in the object "user".
-    {
-        console.log(`${this.username} , welcome to website`);
-        console.log(this);
-    }
+//     welcomeMessage: function()   // { welcomeMessage: function() { ... } is similar to  const welcomeMessage = function() { ... };
+//                                 // here "welcomemesssage " is a key in the object "user".
+//     {
+//         console.log(`${this.username} , welcome to website`);
+//         console.log(this);
+//     }
 
-}
+// }
 
-// user.welcomeMessage()
-// user.price = 0
-// user.welcomeMessage()
+// // user.welcomeMessage()
+// // user.price = 0
+// // user.welcomeMessage()
 
-console.log(this); //   o/p:
-                   //  {}   -> empty object becos In Node.js, the global this refers to {} (an empty object) because Node does not attach variables to global like in browsers.
-                    //   In a browser, this in the global scope refers to the "window object"  ****imp****.
+// console.log(this); //   o/p:
+//                    //  {}   -> empty object becos In Node.js, the global this refers to {} (an empty object) because Node does not attach variables to global like in browsers.
+//                     //   In a browser, this in the global scope refers to the "window object"  ****imp****.
 
 
 // function chai(){
@@ -32,27 +32,42 @@ console.log(this); //   o/p:
 //     console.log(this.username);
 // }
 
-// const chai =  () => {
+// const chai =  () => {        //Behavior of Arrow Functions:  '=>'
+
+//     // Arrow functions do not bind their own this.
+    
+//     // Instead, they inherit this from their surrounding lexical scope (which, in this case, is the global scope).
+    
+//     // In Node.js, this in the global scope is module.exports, an empty object {}.
 //     let username = "hitesh"
 //     console.log(this);
-// }
-
-
+//  } 
 // chai()
 
-// const addTwo = (num1, num2) => {
-//     return num1 + num2
-// }
+const addTwo1 = (num1, num2) => {  //standard arrow function (using {}):
+//     The function takes num1 and num2 as parameters.
+// It uses a block body {}, so you need to explicitly use return to return a value.
+    return num1 + num2
+}
 
-// const addTwo = (num1, num2) =>  num1 + num2
+const addTwo2 = (num1, num2) =>  num1 + num2 // implict return arrow func
+// If the function body is a single expression, you can omit {} and the function automatically returns the expression result.
+// No need for the return keyword.
 
-// const addTwo = (num1, num2) => ( num1 + num2 )
+const addTwo3 = (num1, num2) => ( num1 + num2 ) 
+// The parentheses () around num1 + num2 don't change anything.
+// The function still returns the sum.
 
-const addTwo = (num1, num2) => ({username: "hitesh"})
+const addTwo4 = (num1, num2) => ({username: "hitesh"})
+// This time, the function returns an object { username: "hitesh" }.
+// The parentheses () around the object are necessary because:
+// If you use {} without parentheses, JavaScript thinks it's a function block, not an object.
 
 
-console.log(addTwo(3, 4))
-
+console.log(addTwo1(3, 4))
+console.log(addTwo2(3, 4))
+console.log(addTwo3(3, 4))
+console.log(addTwo4(3, 4))
 
 // const myArray = [2, 5, 3, 7, 8]
 
