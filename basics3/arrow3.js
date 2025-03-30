455074ac15186ded93a8939b922af993ac0e4106
7Narayan <22eg105e38@anurag.edu.in>
2025-03-30 13:22:36 +0530

    learnt1

--- basics3/arrow3.js
+++ basics3/arrow3.js
@@ -44,31 +44,33 @@
 //  } 
 // chai()
 
-const addTwo1 = (num1, num2) => {  //standard arrow function (using {}):
-//     The function takes num1 and num2 as parameters.
-// It uses a block body {}, so you need to explicitly use return to return a value.
-    return num1 + num2
-}
+// const addTwo1 = (num1, num2) => {  //standard arrow function (using {}):
+// //     The function takes num1 and num2 as parameters.
+// // It uses a block body {}, so you need to explicitly use return to return a value.
+//     return num1 + num2
+// }
 
-const addTwo2 = (num1, num2) =>  num1 + num2 // implict return arrow func
-// If the function body is a single expression, you can omit {} and the function automatically returns the expression result.
-// No need for the return keyword.
+// const addTwo2 = (num1, num2) =>  num1 + num2 // implict return arrow func
+// // If the function body is a single expression, you can omit {} and the function automatically returns the expression result.
+// // No need for the return keyword.
 
-const addTwo3 = (num1, num2) => ( num1 + num2 ) 
-// The parentheses () around num1 + num2 don't change anything.
-// The function still returns the sum.
+// const addTwo3 = (num1, num2) => ( num1 + num2 ) 
+// // The parentheses () around num1 + num2 don't change anything.
+// // The function still returns the sum.
 
-const addTwo4 = (num1, num2) => ({username: "hitesh"})
-// This time, the function returns an object { username: "hitesh" }.
-// The parentheses () around the object are necessary because:
-// If you use {} without parentheses, JavaScript thinks it's a function block, not an object.
+// const addTwo4 = (num1, num2) => ({username: "hitesh"})
+// // This time, the function returns an object { username: "hitesh" }.
+// // The parentheses () around the object are necessary because:
+// // If you use {} without parentheses, JavaScript thinks it's a function block, not an object.
 
 
-console.log(addTwo1(3, 4))
-console.log(addTwo2(3, 4))
-console.log(addTwo3(3, 4))
-console.log(addTwo4(3, 4))
+// console.log(addTwo1(3, 4))
+// console.log(addTwo2(3, 4))
+// console.log(addTwo3(3, 4))
+// console.log(addTwo4(3, 4))
 
-// const myArray = [2, 5, 3, 7, 8]
+const myArray = [2, 5, 3, 7, 8]
 
-// myArray.forEach()
+myArray.forEach((i)=>{ // "forEach " is a func, 'i' is a parameter, {} here i snot obj, but func block*.
+    console.log(i);
+});
